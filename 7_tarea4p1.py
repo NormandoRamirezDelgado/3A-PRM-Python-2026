d9bc6dca8c6f8bf9dee33e7494623bab5920c0d3
“Normando” <nramirezf@cbtis236.edu.mx>
2026-09-10 10:18:17 -0600

    Tarea y Relacionales

---
+++ 7_tarea4p1.py
@@ -0,0 +1,13 @@
+''' Hacer un programa que calcule el promedio de 5 calificaciones e imprima el resultado obtenido. '''
+
+calificacionUno = float(input('Introduce calificación: '))
+calificacionDos = float(input('Introduce calificación: '))
+calificacionTres = float(input('Introduce calificación: '))
+calificacionCuatro = float(input('Introduce calificación: '))
+calificacionCinco = float(input('Introduce calificación: '))
+promedio = ( calificacionUno + calificacionDos + calificacionTres + calificacionCuatro + calificacionCinco ) / 5
+
+print(f'El Promedio es: {promedio}')
+
+
+
---
+++ 8_operadores_relacionales.py
@@ -0,0 +1,8 @@
+# comparaciones.py — el resultado de comparar SIEMPRE es True o False
+calificacion = 7
+minima_aprobatoria = 6
+
+print(calificacion >= minima_aprobatoria)   # ¿aprobó?
+print(calificacion == 10)                   # ¿sacó 10 exacto?
+print(calificacion != minima_aprobatoria)   # ¿es distinto del mínimo?
+print(type(calificacion >= minima_aprobatoria))
